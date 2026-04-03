e2537b82834bca965d45255041a228d4a9bd3881
vkshdev <207021168+vkshdev@users.noreply.github.com>
2026-04-03 10:16:38 +0530

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -5,6 +5,7 @@ def positional_encoding(seq_len, d_model, base=10000.0):
     Return PE of shape (seq_len, d_model) using sin/cos formulation.
     Odd d_model -> last column is sin.
     """
+    
     # Write code here
 
     n_sin = (d_model + 1) // 2  
