00908cf9eb7381fe62c182dd8a17c76e5112f5cb
Vikash Kr. <vikashmehta292511@gmail.com>
2026-04-10 19:37:43 +0530

    Add solution for remove-stopwords

---
+++ remove-stopwords/remove-stopwords.py
@@ -0,0 +1,10 @@
+def remove_stopwords(tokens, stopwords):
+    """
+    Returns: list[str] - tokens with stopwords removed (preserve order)
+    """
+    # Your code here
+
+    stopword_set = set(stopwords)
+    return [token for token in tokens if token not in stopword_set]
+    
+    pass
